2cee91f3a23ba6531a4d990bd683c8b3d169011a
DiegoRondonR <furrtr4532@gmail.com>
2025-04-18 18:28:23 -0500

    feat: actualizar autorización en LeadsController y registrar LeadService en LeadModule

--- src/Controllers/Lead/LeadController.cs
+++ src/Controllers/Lead/LeadController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace GestionHogar.Controllers;
 
 [ApiController]
-[Authorize(Roles = "SuperAdmin")]
+[Authorize]
 [Route("api/[controller]")]
 public class LeadsController : ControllerBase
 {
