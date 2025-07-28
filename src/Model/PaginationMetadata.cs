b294fd25eb0061b76d0f225ea363ed7cfdb32ea5
DiegoRondonR <furrtr4532@gmail.com>
2025-07-27 20:15:49 -0500

    feat: agregar clases para paginación con respuesta paginada y metadatos

---
+++ src/Model/PaginationMetadata.cs
@@ -0,0 +1,11 @@
+namespace GestionHogar.Model;
+
+public class PaginationMetadata
+{
+    public int Total { get; set; }
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int TotalPages { get; set; }
+    public bool HasNext { get; set; }
+    public bool HasPrevious { get; set; }
+}
